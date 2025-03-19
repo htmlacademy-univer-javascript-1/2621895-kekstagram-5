3679510db117554567c205e50ab5b22a57027800
G0s3 <ilyasapozhnikov@inbox.ru>
2025-03-19 20:47:22 +0500

    4.16. Больше деталей

---
+++ js/functions.js
@@ -0,0 +1,45 @@
+function stringLength(str, maxLenght){
+  return str.length <= maxLenght;
+}
+
+stringLength('проверяемая строка', 20);
+stringLength('проверяемая строка', 18);
+stringLength('проверяемая строка', 10);
+
+
+function palindrome(str){
+  const normalizedStr = str.replaceAll(' ', '').toLowerCase();
+  let newStr = '';
+  for (let i = normalizedStr.length - 1; i >= 0; i--) {
+    newStr += normalizedStr[i];
+  }
+  return(normalizedStr === newStr);
+}
+
+palindrome('topot');
+
+
+function numbersInRow(row){
+  if (typeof row === 'number') {
+    row = row.toString();
+  } else if (typeof row !== 'string') {
+    return NaN;
+  }
+
+  let newStr = '';
+
+  for (let i = 0; i < row.length; i++){
+    const num = parseInt(row[i], 10);
+    if (!isNaN(num)) {
+      newStr += row[i];
+    }
+  }
+
+  return newStr.length > 0 ? parseInt(newStr, 10) : NaN;
+}
+
+numbersInRow('2023 year');
+numbersInRow('ECMAScript 2022');
+numbersInRow(2023);
+numbersInRow(-1);
+numbersInRow(1.5);
