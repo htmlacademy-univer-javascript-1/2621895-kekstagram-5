ecd0b2b7ca3ffb5e8ef74f32abbaf920cd973ca1
G0s3 <ilyasapozhnikov@inbox.ru>
2025-04-19 16:49:01 +0500

    module7-task1

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import { generatePhotos } from './data.js';
+
+// Генерация массива фотографий и вывод в консоль
+const photosData = generatePhotos();
+photosData(); //чтобы еслинт не ругался
+//console.log(photosData);
